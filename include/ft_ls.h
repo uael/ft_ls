b0ce8584f2b24f4d40541062d523801e0b8af36f
alucas- <alucas-@student.42.fr>
2017-11-16 19:07:53 +0100

    opts parsing

--- include/ft_ls.h
+++ include/ft_ls.h
@@ -15,4 +15,20 @@
 
 # include <libft.h>
 
+typedef enum	e_ls_opts
+{
+	FT_LS_DOTS = 1 << 0,
+	FT_LS_LONG = 1 << 1,
+	FT_LS_RECU = 1 << 2,
+	FT_LS_REVE = 1 << 2,
+	FT_LS_ASCT = 1 << 3
+}				t_ls_opts;
+
+typedef	struct	s_ls_ctx
+{
+	char		*prg;
+	uint8_t		opts;
+	t_vstr		files;
+}				t_ls_ctx;
+
 #endif
--- src/ft_ls.c
+++ src/ft_ls.c
@@ -12,9 +12,54 @@
 
 #include "ft_ls.h"
 
+uint8_t	ls_usage(t_ls_ctx *ctx, char opt)
+{
+	ft_putstr(ctx->prg);
+	ft_putstr(": illegal option -- ");
+	ft_putchar(opt);
+	ft_putchar('\n');
+	ft_putstr("usage: ");
+	ft_putstr(ctx->prg);
+	ft_putstr(" [-Ralrt] [file ...]");
+	return (1);
+}
+
+uint8_t	ls_parse_opts(t_ls_ctx *ctx, int ac, char **av)
+{
+	int		i;
+	char	*arg;
+
+	i = -1;
+	while (++i < ac && (arg = av[i]))
+		if (*arg != '-' && !ft_vstr_pushc(&ctx->files, arg))
+			return (1);
+		else if (*arg == '-')
+			while (++*arg)
+				if (*arg == 'a')
+					ctx->opts |= FT_LS_DOTS;
+				else if (*arg == 'l')
+					ctx->opts |= FT_LS_LONG;
+				else if (*arg == 'R')
+					ctx->opts |= FT_LS_RECU;
+				else if (*arg == 'r')
+					ctx->opts |= FT_LS_REVE;
+				else if (*arg == 't')
+					ctx->opts = FT_LS_ASCT;
+				else if (*arg)
+					return (ls_usage(ctx, *arg));
+	return (0);
+}
+
 int		main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	t_ls_ctx ctx;
+
+	FT_INIT(&ctx, t_ls_ctx);
+	if ((ctx.prg = ft_strrchr(av[0], '/')))
+		++ctx.prg;
+	else
+		ctx.prg = av[0];
+	if (ls_parse_opts(&ctx, ac, av))
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
