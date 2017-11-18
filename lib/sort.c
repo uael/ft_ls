ff6b465b0fee2e40b17577ada6c7633d74aaba7e
uael <lucas.abel@yandex.com>
2017-11-18 20:04:15 +0100

    func name cleanup

--- lib/sort.c
+++ lib/sort.c
@@ -6,23 +6,23 @@
 /*   By: alucas- <alucas-@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 09:52:30 by alucas-           #+#    #+#             */
-/*   Updated: 2017/11/18 19:43:27 by null             ###   ########.fr       */
+/*   Updated: 2017/11/18 20:03:31 by null             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ls.h"
 
-static int	ls_ent_sort_alpha(t_ls_entry *a, t_ls_entry *b)
+static int	ls_entry_strcmp(t_ls_entry *a, t_ls_entry *b)
 {
 	return (ft_strcmp(a->path, b->path));
 }
 
-static int	ls_ent_sort_time(t_ls_entry *a, t_ls_entry *b)
+static int	ls_entry_timecmp(t_ls_entry *a, t_ls_entry *b)
 {
 	int r;
 
 	r = -I64CMP(a->stat.st_ctime, b->stat.st_ctime);
-	return (r != 0 ? r : ls_ent_sort_alpha(a, b));
+	return (r != 0 ? r : ls_entry_strcmp(a, b));
 }
 
 
@@ -39,10 +39,10 @@ void		ls_entry_sort(t_ls_entry *self, size_t n, uint8_t opt)
 		while (++i < n)
 		{
 			if ((opt & LS_ASCT) &&
-				(c = ls_ent_sort_time(self + i - 1, self + i)) == 0)
+				(c = ls_entry_timecmp(self + i - 1, self + i)) == 0)
 				continue ;
 			if (!(opt & LS_ASCT) &&
-				(c = ls_ent_sort_alpha(self + i - 1, self + i)) == 0)
+				(c = ls_entry_strcmp(self + i - 1, self + i)) == 0)
 				continue ;
 			if ((opt & LS_REVE) && c > 0)
 				continue ;
